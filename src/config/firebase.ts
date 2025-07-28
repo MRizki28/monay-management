7d47c6304039c1f537b03623622cccdaf1b5aeee
Rizki <muhammadrizkitkj123@gmail.com>
2025-07-28 15:44:10 +0800

    feat:update

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,16 +8,16 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAZ111TiLaeOlKUhgPT5J9_rkCyFrbd5WI",
-    authDomain: "tabungan-nikah.firebaseapp.com",
-    databaseURL: "https://tabungan-nikah-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "tabungan-nikah",
-    storageBucket: "tabungan-nikah.firebasestorage.app",
-    messagingSenderId: "344974375328",
-    appId: "1:344974375328:web:0cf90242043c0593c1ccc5",
-    measurementId: "G-0P28E4R49K"
-};
-
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  };
+  
 // Initialize Firebase
 const database = getDatabase(initializeApp(firebaseConfig));
 
